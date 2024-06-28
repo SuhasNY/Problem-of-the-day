c645b359514ca3c527797e600bb538d834eec643
SuhasNY <156566270+SuhasNY@users.noreply.github.com>
2024-06-28 22:20:30 +0530

    Add files via upload

---
+++ 28-6-24--Palindrome-pattern.py
@@ -0,0 +1,17 @@
+# def pattern(array):
+#     n = len(array)
+    
+#     def is_palindrome(list):
+#         if (list == list[::-1]):
+#             return True
+#         else:
+#             return False
+        
+#     for i in range(0, n):
+#         if is_palindrome(array[i]):
+#             print(i, ' R')
+#             break
+    
+
+list = [[0,1,2],[4,5,6]]
+print(list[:][0])
